b92c071df25d19928fc3b89a9018bfd4a687a9ac
Matthew Podwysocki <matthewp@microsoft.com>
2022-12-06 14:11:40 -0500

    Initial Commit

---
+++ NotificationHubs/NotificationHubs.h
@@ -0,0 +1,18 @@
+//
+//  NotificationHubs.h
+//  NotificationHubs
+//
+//  Created by Matthew Podwysocki on 12/6/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NotificationHubs.
+FOUNDATION_EXPORT double NotificationHubsVersionNumber;
+
+//! Project version string for NotificationHubs.
+FOUNDATION_EXPORT const unsigned char NotificationHubsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotificationHubs/PublicHeader.h>
+
+
